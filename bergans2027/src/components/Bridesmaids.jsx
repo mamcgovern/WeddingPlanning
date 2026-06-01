1f4060b80d16b4723192f79a6c32d1cb7d03c543
Maddelynne McGovern <maddelynnemcgovern@gmail.com>
2026-06-01 09:01:05 -0500

    Basic site navigation set up

---
+++ bergans2027/src/components/Bridesmaids.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Bridesmaids() {
+    return <div>Bridesmaids Page</div>
+}
---
+++ bergans2027/src/components/Groomsmen.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Groomsmen() {
+    return <div>Groomsmen Page</div>
+}
