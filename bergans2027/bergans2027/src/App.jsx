2083c149467e5129c025586f3f55f204d9faf358
Maddelynne McGovern <maddelynnemcgovern@gmail.com>
2026-06-01 16:22:23 -0500

    merging branch 3

--- bergans2027/bergans2027/src/App.jsx
+++ bergans2027/bergans2027/src/App.jsx
@@ -5,18 +5,21 @@ import ScrollToTop from './components/ScrollToTop.jsx'
 
 import Home from './components/Home.jsx'
 import Events from './components/Events.jsx'
-import Bach from './components/events/Bach.jsx'
-import Rehearsal from './components/events/Rehearsal.jsx'
-import WeddingDay from './components/events/WeddingDay.jsx'
-import Bridesmaids from './components/Bridesmaids.jsx'
-import Groomsmen from './components/Groomsmen.jsx'
+    import Bach from './components/events/Bach.jsx'
+    import Rehearsal from './components/events/Rehearsal.jsx'
+    import WeddingDay from './components/events/WeddingDay.jsx'
+import WeddingParty from './components/WeddingParty.jsx'
+    import Parents from './components/weddingParty/Parents.jsx'
+    import Bridesmaids from './components/weddingParty/Bridesmaids.jsx'
+    import Groomsmen from './components/weddingParty/Groomsmen.jsx'
 import Timeline from './components/Timeline.jsx'
+
 
 export default function App() {
     return (
         <>
             <ScrollToTop />
-            
+
             <Header />
 
             <main className="page-container py-4">
@@ -30,11 +33,16 @@ export default function App() {
                     <Route path="/events/rehearsal" element={<Rehearsal />} />
                     <Route path="/events/wedding" element={<WeddingDay />} />
 
+                    {/* Wedding Party */}
+                    <Route path="/weddingparty" element={<WeddingParty />} />
+                    <Route path="/weddingparty/parents" element={<Parents />} />
+                    <Route path="/weddingparty/bridesmaids" element={<Bridesmaids />} />
+                    <Route path="/weddingparty/groomsmen" element={<Groomsmen />} />
                     {/* Bridesmaids */}
-                    <Route path="/bridesmaids" element={<Bridesmaids />} />
+
 
                     {/* Groomsmen */}
-                    <Route path="/groomsmen" element={<Groomsmen />} />
+
 
                     {/* Timeline */}
                     <Route path="/timeline" element={<Timeline />} />
