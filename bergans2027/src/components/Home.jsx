00c1c0c4b4b7093e29b66e0abe036cdd09c508eb
Maddelynne McGovern <maddelynnemcgovern@gmail.com>
2026-06-01 09:45:57 -0500

    Created timeline component for home page

--- bergans2027/src/components/Home.jsx
+++ bergans2027/src/components/Home.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
+import Timeline from './Timeline.jsx'
 
 export default function Home() {
-    return <div>Home Page</div>
+    return <div>
+        Home Page
+        <Timeline />
+    </div>
 }
