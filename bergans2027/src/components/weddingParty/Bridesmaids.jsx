cac57890a3ed180930a0309c405a2e46d68c9868
Maddelynne McGovern <maddelynnemcgovern@gmail.com>
2026-07-13 12:33:53 -0500

    updated bridesmaids & groomsmen info

--- bergans2027/src/components/weddingParty/Bridesmaids.jsx
+++ bergans2027/src/components/weddingParty/Bridesmaids.jsx
@@ -1,29 +1,121 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import BridesmaidInfo from './bridesmaids/BridesmaidsInfo'
-import BridesmaidAttire from './bridesmaids/BridesmaidAttire'
-import BridesmaidChecklist from './bridesmaids/BridesmaidChecklist'
+import dressesData from '../../data/bridesmaids/bridesmaidAttire.json'
+
+function DressCard({ dress }) {
+  return (
+    <div className="card">
+
+      <div className="card-image">
+        <img src={dress.photo} alt={dress.name} />
+      </div>
+
+      <div className="card-info">
+        <div className="card-name">{dress.name}</div>
+
+        <a
+          href={dress.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="card-btn"
+        >
+          View Dress
+        </a>
+      </div>
+
+    </div>
+  )
+}
 
 export default function Bridesmaids() {
-    return (
+  const sageDresses = dressesData?.sage || []
+  const blushDresses = dressesData?.blush || []
+  const shoes = dressesData?.shoes || []
+
+  return (
+    <div>
+
+      <div>
+
+        <h1>Dresses</h1>
+
+        <p className="subtitle">
+          Dress colors have been assigned to the bridesmaids.
+          You can purchase any chiffon dress from the Birdy Grey website
+          in your assigned color, including, but not limited to the following.
+        </p>
+
         <div>
-            <h1>Bridesmaids</h1>
 
-            <p className="subtitle">
-                Meet the bridesmaids and find everything you need for the wedding day.
-            </p>
-            
-            <h2>Meet the Maids</h2>
-            <BridesmaidInfo />
-            <hr></hr>
+          <h2>Sage</h2>
+          <p className='subtitle'>Assigned to: Sydni, Emily, Mary, Kay</p>
 
-            <h2>Attire</h2>
-            <BridesmaidAttire />
-            <hr></hr>
+          <div className="grid2">
+            {sageDresses.map((dress) => (
+              <DressCard key={dress.name + dress.link} dress={dress} />
+            ))}
+          </div>
 
-            <h2>Checklist</h2>
-            <BridesmaidChecklist />
+          <button
+            className="pink-btn"
+            onClick={() =>
+              window.open(
+                'https://www.birdygrey.com/collections/green-bridesmaid-dresses?filter.tags_fabric=chiffon&filter.tags_color_bar=sage',
+                '_blank'
+              )
+            }
+          >
+            View More
+          </button>
 
         </div>
-    )
+
+        <div>
+          <h2>Blush Pink</h2>
+          <p className="subtitle">Assigned to: Lizzy, Taylor, Toph</p>
+          <div className="grid2">
+            {blushDresses.map((dress) => (
+              <DressCard key={dress.name + dress.link} dress={dress} />
+            ))}
+          </div>
+          <button
+            className="pink-btn"
+            onClick={() =>
+              window.open(
+                'https://www.birdygrey.com/collections/bridesmaid-dresses?filter.tags_fabric=chiffon&filter.tags_color_bar=blush+pink',
+                '_blank'
+              )
+            }
+          >
+            View More
+          </button>
+        </div>
+      </div>
+      <div>
+        <h2>Shoes</h2>
+        <p>
+          The bridesmaids are welcome to wear any nude or tan dress shoe of their choice,
+          including (but not limited to) styles similar to those shown below.
+          Heels are not required.
+        </p>
+
+        <div className="grid2">
+          {shoes.map((shoe) => (
+            <DressCard key={shoe.name + shoe.link} dress={shoe} />
+          ))}
+        </div>
+
+        <button
+          className="pink-btn"
+          onClick={() =>
+            window.open(
+              'https://www.dsw.com/category/womens/shoes/dress?color=Beige',
+              '_blank'
+            )
+          }
+        >
+          View More
+        </button>
+      </div>
+    </div>
+  )
 }
--- bergans2027/src/components/weddingParty/Groomsmen.jsx
+++ bergans2027/src/components/weddingParty/Groomsmen.jsx
@@ -1,29 +1,117 @@
+
 import React from 'react'
-import { Link } from 'react-router-dom'
-import GroomsmenInfo from './groomsmen/GroomsmenInfo'
-import GroomsmenAttire from './groomsmen/GroomsmenAttire'
-import GroomsmenChecklist from './groomsmen/GroomsmenChecklist'
+import attireData from '../../data/groomsmen/groomsmenAttire.json'
 
-export default function Groomsmens() {
+function Card({ item }) {
     return (
-        <div className="container">
-            <h1>Groomsmen</h1>
+        <div className="card">
 
-            <p className="subtitle">
-                Meet the groomsmen and find everything you need for the wedding day.
-            </p>
+            <div className="card-image">
+                <img src={item.photo} alt={item.name} />
+            </div>
 
-            <h2>Meet the Men</h2>
-            <GroomsmenInfo />
-            <hr></hr>
+            <div className="card-info">
+                <div className="card-name">{item.name}</div>
 
-            <h2>Attire</h2>
-            <GroomsmenAttire />
-            <hr></hr>
+                <a
+                    href={item.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="card-btn"
+                >
+                    View
+                </a>
+            </div>
 
-            <h2>Checklist</h2>
-            <GroomsmenChecklist />
+        </div>
+    )
+}
 
+function DescriptionCard({ name, photo, description, link }) {
+    return (
+        <div className="card">
+            <div className="card-image">
+                <img src={photo} alt={name} />
+            </div>
+
+            <div className="card-info">
+                <div className="card-name">{name}</div>
+                <div className="card-description">{description}</div>
+
+                <a
+                    href={link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="card-btn"
+                >
+                    View
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default function Groomsmen() {
+    const suit = attireData?.suit || []
+    const shoes = attireData?.shoes || []
+    const sageTie = attireData?.sage || []
+    const blushTie = attireData?.blush || []
+
+    return (
+        <div>
+            {/* Suit */}
+            <div>
+                <h1>Suit</h1>
+                <div className="grid1">
+                    {suit.map((item) => (
+                        <Card key={item.name + item.link} item={item} />
+                    ))}
+                </div>
+            </div>
+
+            {/* Shoes */}
+            <div>
+                <h1>Shoes</h1>
+                <p className='subtitle'>The groomsmen are welcome to wear any black dress shoe they would like, including, but not limited to the following.</p>
+
+                <div className="grid2">
+                    {shoes.map((item) => (
+                        <Card key={item.name + item.link} item={item} />
+                    ))}
+                </div>
+
+                <button
+                    className="pink-btn"
+                    onClick={() =>
+                        window.open(
+                            'https://www.shoecarnival.com/mens/dress?colorPrimary=BLACK',
+                            '_blank'
+                        )
+                    }
+                >
+                    View More
+                </button>
+            </div>
+
+            {/* Ties */}
+            <div>
+                <h1>Ties</h1>
+                <p className='subtitle'>Tie colors have been assigned to the groomsmen, and the ties were gifted as a part of the groomsmen's gift boxes.</p>
+                <div className="grid2">
+                    <DescriptionCard
+                        name="Simon Tie - Sage Green"
+                        photo="https://cdn.shopify.com/s/files/1/2053/3799/files/sage_simon_tie_1.jpg.webp?v=1741985302&width=1200&height=1800&crop=center&quality=85"
+                        description="Assigned to: Nathan, Quentin, Damien, John"
+                        link="https://www.birdygrey.com/products/simon-necktie-sage"
+                    />
+                    <DescriptionCard
+                        name="Simon Tie - Blush Pink"
+                        photo="https://cdn.shopify.com/s/files/1/2053/3799/files/blush_pink_simon_tie_1.jpg.webp?v=1741983461&width=1200&height=1800&crop=center&quality=85"
+                        description="Assigned to: Kurt, Sam, Henry"
+                        link="https://www.birdygrey.com/products/simon-necktie-blush-pink"
+                    />
+                </div>
+            </div>
         </div>
     )
 }
