d07c085e35fd86217195b1745467b65780ca7071
Maddelynne McGovern <maddelynnemcgovern@gmail.com>
2026-08-10 10:07:14 -0500

    working on redesigning website

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import couple1 from './assets/engagement-photo-1.jpg'
+import couple2 from './assets/engagement-photo-2.jpg'
+import couple3 from './assets/engagement-photo-3.jpg'
+import couple4 from './assets/engagement-photo-4.jpg'
+
+import "./index.css";
+
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <main>
+        Hi
+      </main>
+    </div>
+  )
+}
+
+export default App
